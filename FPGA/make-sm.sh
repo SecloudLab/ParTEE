ef60087fd6898c2eeb6dd1f2ef60a5eea9d19a29
alan2035 <lanhao20s@ict.ac.cn>
2025-01-27 09:26:17 +0800

    Update make-sm.sh

--- FPGA/make-sm.sh
+++ FPGA/make-sm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-make -C opensbi O=$(pwd)/zza-tmp-build KEYSTONE_PLATFORM=vivado-risc-v PLATFORM_DIR=$(pwd)/plat/vivado-risc-v CROSS_COMPILE=/opt/riscv64/bin/riscv64-unknown-linux-gnu- FW_PAYLOAD_PATH=`realpath ./u-boot-nodtb.bin`
+make -C opensbi O=$(pwd)/build PLATFORM=vivado PLATFORM_DIR=$(pwd)/plat/vivado CROSS_COMPILE=/opt/riscv64/bin/riscv64-unknown-linux-gnu- FW_PAYLOAD_PATH=`realpath ./u-boot-nodtb.bin`
 
 
